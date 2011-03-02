994349e44e012b91264f733a4b83a88db8188e71
hetoan2 <hetoan2@gmail.com>
2011-03-01 21:12:33 -0500

    v1.2
    Added player glow on zoom. Idea taken from Metasmash (no he didn't help me or approve of this ._.)

--- variable-scope.asm
+++ variable-scope.asm
@@ -1,26 +1,27 @@
 /*zoom modifier ASM by hetoan2*/
 
-.long 0x20A2EEA4, 0x00000001	# fix for wifi bug
+.long 0x214EDC14, 0x0000000C	#fix for wifi bug
 
-.set rambase,0x81656000			# don't mess with unless you know what you're doing!
+.set rambase,0x81656000			#don't mess with unless you know what you're doing!
 .set activator,0x80200F0A
 
-# comment the line below (add # to the front) to use WiiMote + Nunchuck
+#comment the line below (add # to the front) to use WiiMote + Nunchuck
 .set ccpro,1
 .ifndef ccpro
 .set activator,0x80200EE0
 .endif
 
 #button values
-.set buttoninc,0x2001	# increase zoom button (L + up)
-.set buttondec,0x6000	# decrease zoom button (L + down)
+.set buttonscope,0x2000	#scope button (L)
+.set buttoninc,0x0001	#increase zoom button (up)
+.set buttondec,0x4000	#decrease zoom button (down)
 
 .int rambase<<7>>7|0x04000008
-.float .05				# increment speed variable
+.float .05				#increment speed variable
 .int rambase<<7>>7|0x0400000C
-.float 1				# minimum zoom variable
+.float 1				#minimum zoom variable
 .int rambase<<7>>7|0x04000010
-.float 15				# maximum zoom variable
+.float 15				#maximum zoom variable
 
 .set codeaddress,0x8074B810
 .set length,end1-start1
@@ -29,9 +30,7 @@
 .int codeaddress<<7>>7|0xC2000000
 .int numlines
 
-.set fdefy,0x3F969DC1
-#setreg
-
+.set fdefy,0x3F969DC1	#sets default y value zoom (may need changing if issues occur)
 .set fbase,17
 
 start1:
@@ -92,7 +91,7 @@ end2:
 .int align
 .balignl 8,0
 
-#controller
+#controller start
 
 .set length,end3-start3
 .set align,(length%8==0)*-0x60000000
@@ -108,12 +107,46 @@ stmw r14,8(r1)			#load r14-r31 into stackframe
 
 lis r30,activator@h
 ori r30,r30,activator@l
-lhz r31,0(r30)
+lhz r31,0(r30)			#load buttonpressed
+
+cmpwi r31,buttonscope
+beq do_glow
+cmpwi r31,buttoninc+buttonscope
+beq do_glow
+cmpwi r31,buttondec+buttonscope
+beq do_glow
+b skip_glow
+
+.set baseglow,0x8144EC20
+.set glowred,0x3F24A4A0
+.set glowgreen,0x3D007FED
+.set glowblue,0x3C007FED
+
+do_glow:
+lis r24,baseglow@h
+ori r24,r24,baseglow@l
+li r23,0
+startglow:
+lis r20,glowred@h
+ori r20,r20,glowred@l
+lis r21,glowgreen@h
+ori r21,r21,glowgreen@l
+lis r22,glowblue@h
+ori r21,r21,glowblue@l
+addi r23,r23,1
+stw r20,0(r24)
+stw r21,0x10(r24)
+stw r22,0x20(r24)
+addi r24,r24,0x78		#offset to next player
+cmpwi r23,9
+blt startglow			#loop for all 9 players
+
+skip_glow:
 lis r17,rambase@h
 ori r17,r17,rambase@l
-cmpwi r31,buttoninc
+cmpwi r31,buttoninc+buttonscope
 beq up
-cmpwi r31,buttondec
+cmpwi r31,buttondec+buttonscope
 bne store
 b down
 
@@ -155,4 +188,4 @@ end3:
 .int align
 .balignl 8,0
 
-.long 0xE0000000, 80008000
+.long 0xE0000000, 0x80008000
